202f7372a709e9f6d1a20ae64db25bf643d9a7fe
farrux <kfarrux2005@gmail.com>
2022-08-10 20:14:25 +0500

    hellow

---
+++ models/users.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      match: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    userRole: {
+      type: Number,
+      default: 0
+    },
+    phoneNumber: {
+      type: Number,
+    },
+    userImage: {
+      type: String,
+      default: "user.png",
+    },
+    verified: {
+      type: String,
+      default: false,
+    },
+    secretKey: {
+      type: String,
+      default: null,
+    },
+    history: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model("users", userSchema);
+module.exports = userModel;
